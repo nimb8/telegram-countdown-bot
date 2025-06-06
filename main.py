e2c1c8edf5f175d1418a62cbe06d79728184259c
nimb8 <46843817+nimb8@users.noreply.github.com>
2025-06-06 15:18:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ import asyncio
 from datetime import datetime, timedelta, time, timezone
 from telegram import Bot
 
-now = datetime.now(timezone.utc)
-print("Текущее время UTC:", now.isoformat())
-
 BOT_TOKEN = "7857747352:AAGL6XXQyZlj-6k_U6BV-rFF2wacDRdGjVE"  # или os.getenv("BOT_TOKEN")
 CHAT_ID = "-4874063053"        # или os.getenv("CHAT_ID")
 
-VACATION_DATE = datetime(2025, 7, 27, 12, 14, 0, tzinfo=timezone.utc)  # дата и время отпуска (UTC)
+VACATION_DATE = datetime(2025, 7, 27, 15, 0, 0, tzinfo=timezone.utc)  # дата и время отпуска (UTC)
 
 async def send_countdown():
     bot = Bot(token=BOT_TOKEN)
@@ -22,7 +19,7 @@ async def send_countdown():
             print("Отправлено: Отпуск начинается!!!")
             break
 
-        # Рассчитаем сколько осталось
+        # Вычисляем сколько осталось
         diff = VACATION_DATE - now
 
         days = diff.days
@@ -32,22 +29,19 @@ async def send_countdown():
         seconds = seconds % 60
 
         message = f"До отпуска осталось: {days} дн {hours} ч {minutes} м {seconds} с"
+        await bot.send_message(chat_id=CHAT_ID, text=message)
+        print("Отправлено:", message)
 
-        # Определяем момент следующей отправки — 14:53 UTC каждый день
-        target_time_today = datetime.combine(now.date(), time(12, 15, 0, tzinfo=timezone.utc))
+        # Рассчитаем сколько секунд ждать до следующего 11:00 UTC
+        target_time_today = datetime.combine(now.date(), time(15, 0, 0, tzinfo=timezone.utc))
         if now < target_time_today:
             wait_seconds = (target_time_today - now).total_seconds()
-            # Ждем до target_time_today, не отправляем сообщение сразу
-            print(f"Ждем до {target_time_today.isoformat()} (еще {wait_seconds:.0f} секунд)")
-            await asyncio.sleep(wait_seconds)
-            await bot.send_message(chat_id=CHAT_ID, text=message)
-            print("Отправлено:", message)
         else:
-            # Если уже после target_time_today, ждем до следующего дня
+            # если уже позже 11:00, ждём до завтра 11:00
             target_time_tomorrow = target_time_today + timedelta(days=1)
             wait_seconds = (target_time_tomorrow - now).total_seconds()
-            print(f"Ждем до {target_time_tomorrow.isoformat()} (еще {wait_seconds:.0f} секунд)")
-            await asyncio.sleep(wait_seconds)
+
+        await asyncio.sleep(wait_seconds)
 
 if __name__ == "__main__":
     asyncio.run(send_countdown())
