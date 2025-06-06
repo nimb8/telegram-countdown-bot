9e0e0d8206d51855a2127d38ffdfe767e4087b51
nimb8 <46843817+nimb8@users.noreply.github.com>
2025-06-06 13:42:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import datetime
 import requests
 import os
+import sys
 
 app = Flask(__name__)
 
@@ -40,16 +41,21 @@ def scheduler(token, chat_id):
         now = datetime.datetime.utcnow()
         if now.hour == 11 and now.minute == 0:
             send_message(token, chat_id)
-            time.sleep(60)  # Ждём минуту, чтобы не отправлять несколько раз за одну минуту
+            time.sleep(60)  # чтобы не отправлять несколько раз в одну минуту
         else:
-            time.sleep(30)  # Проверяем каждые 30 секунд
+            time.sleep(30)  # проверяем каждые 30 секунд
 
 if __name__ == "__main__":
+    print("Переменные окружения в контейнере:")
+    for k, v in os.environ.items():
+        print(f"{k} = {v}")
+
     TOKEN = os.getenv("BOT_TOKEN")
     CHAT_ID = os.getenv("CHAT_ID")
 
     if not TOKEN or not CHAT_ID:
-        raise ValueError("Отсутствуют переменные окружения BOT_TOKEN или CHAT_ID")
+        print("Ошибка: Не заданы переменные окружения BOT_TOKEN или CHAT_ID")
+        sys.exit(1)
 
     threading.Thread(target=scheduler, args=(TOKEN, CHAT_ID), daemon=True).start()
 
